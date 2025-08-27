d05d5cf2762ce297ba425744e34bcda786aad6da
AdityaSharma0410 <melee.zeno@gmail.com>
2025-08-27 17:23:38 +0530

    Added Spring Stack for Backend with all the required dependencies

---
+++ backend/src/main/java/com/etherealcart/backend/EtherealCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.etherealcart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtherealCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtherealCartBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/etherealcart/backend/EtherealCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etherealcart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtherealCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
